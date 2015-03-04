8ddb38106e0f1d20b6b24227183362b6155a6c76
Murugeswari <spoorni95@gmail.com>
2015-03-04 14:54:12 +0530

    this is the second item selection class

---
+++ SecondItemSelection.java
@@ -0,0 +1,10 @@
+package com.example.androidspinnerexample;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Toast;
+
+public abstract class MysecoundItemSelectedListener implements OnItemSelectedListener
+{
+
